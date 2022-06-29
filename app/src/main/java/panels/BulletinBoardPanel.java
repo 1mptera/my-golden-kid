43b0be0702694a50fed958ed7e158ce4dd98ec6c
Jeon Minji <mjjeon2645@gmail.com>
2022-06-29 13:55:00 +0900

    글쓰기 버튼을 클릭하여 글을 쓰면 게시판에 글 제목이 누적됨
    1. 글쓰기 버튼을 클릭 후 글쓰기 완료를 하면 게시판에 글 제목이 누적됨
    2. 누적되는 글 만큼 gridlayout 행 개수가 늘어남

--- app/src/main/java/panels/BulletinBoardPanel.java
+++ app/src/main/java/panels/BulletinBoardPanel.java
@@ -1,21 +1,35 @@
 package panels;
 
-import models.Posting;
 import repositories.PostingRepository;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.List;
 
 public class BulletinBoardPanel extends JPanel {
+  private PostingRepository postingRepository;
 
-  public BulletinBoardPanel() {
+  public BulletinBoardPanel(PostingRepository postingRepository) {
+    this.postingRepository = postingRepository;
+
+    this.setLayout(new GridLayout(postingRepository.postingsSize() + 1, 1));
 
      // 헤드라인
+    initHeadline();
+
+    // 게시판 글 누적 섹션
+    initPostingListsSection();
+  }
+
+  public void initHeadline() {
     JLabel headline = new JLabel("오늘의 식단 자랑하기");
     this.add(headline);
+  }
 
-    // TODO: 게시판 글 누적하여 여기에 보여주기
-
+  // TODO: 게시판 글 누적하여 여기에 보여주기
+  public void initPostingListsSection() {
+    for (int i = postingRepository.postingsSize() - 1; i >= 0; i -= 1) {
+      JLabel post1 = new JLabel(postingRepository.showTitle(i));
+      this.add(post1);
+    }
   }
 }
--- app/src/main/java/repositories/PostingRepository.java
+++ app/src/main/java/repositories/PostingRepository.java
@@ -6,42 +6,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PostingRepository {
-  private List<Posting> postings = new ArrayList<>();
-  private List<String> identifiers = new ArrayList<>();
-  private List<String> passwords = new ArrayList<>();
-  private List<String> titles = new ArrayList<>();
-  private List<String> contents = new ArrayList<>();
+  private final List<Posting> postings;
 
   public PostingRepository() {
+    postings = new ArrayList<>();
     List.of(
       new Posting("봄이엄마", "1234", "오늘 메뉴 짱이예요", "봄이랑 정말 맛있게 먹은 메뉴!! 부럽죠? ㅎㅎ"),
       new Posting("봄이아빠", "2345", "봄이네 식구 행복한 한끼얌얌!", "아이 생일상을 한끼얌얌으로 챙겨줬어요")
-    ).stream().forEach(post -> {
-      postings.add(post);
-    });
+    );
+}
+
+  public void addPosting(String identifier, String password, String title, String text) {
+    postings.add(
+        new Posting(identifier, password, title, text)
+    );
   }
 
-//
-//  public Posting find(String identifier) {
-//    return postings.stream()
-//        .filter(id -> id.identifier().equals(identifier))
-//        .findFirst()
-//        .get();
-//  }
-
-  public void getIdentifier(String identifier) {
-    identifiers.add(identifier);
+  public int postingsSize() {
+    return postings.size();
   }
 
-  public void getPassword(String password) {
-    passwords.add(password);
+  public Posting showPost(int index) {
+    return postings.get(index);
   }
 
-  public void getTitle(String title) {
-    titles.add(title);
-  }
-
-  public void getContent(String content) {
-    contents.add(content);
+  public String showTitle(int index) {
+    return postings.get(index).title();
   }
 }
