c67bacc2f739b89bbeafa46367eec1c75b55f229
Jeon Minji <mjjeon2645@gmail.com>
2022-07-01 00:59:26 +0900

    중복코드 매서드화, 변수명 조정
    1. 중복되는 코드를 묶어 메소드로 만듦
    2. 변수명이 적절하지 않거나 제대로 부여되지 않았을 경우 조정

--- app/src/main/java/popups/DetailsPopUp.java
+++ app/src/main/java/popups/DetailsPopUp.java
@@ -47,36 +47,34 @@ public class DetailsPopUp {
   }
 
   public void initWriterSection() {
-    JLabel identifier = new JLabel("작성자 id");
+    JLabel identifier = new JLabel("작성자");
     identifier.setBounds(50, 20, 50, 50);
+    detailsPanel.add(identifier);
 
     identifierBox = new JTextField(20);
     identifierBox.setText(post.identifier());
     identifierBox.setEditable(false);
     identifierBox.setBounds(110, 30, 150, 30);
-    detailsPanel.add(identifier);
     detailsPanel.add(identifierBox);
   }
 
   public void initPasswordSection() {
     JLabel password = new JLabel("비밀번호");
     password.setBounds(50, 70, 100, 30);
+    detailsPanel.add(password);
 
     passwordBox = new JTextField(4);
     passwordBox.setBounds(110, 70, 250, 30);
-
-    detailsPanel.add(password);
     detailsPanel.add(passwordBox);
   }
 
   public void initTitleSection() {
     JLabel title = new JLabel("제목");
     title.setBounds(50, 110, 30, 30);
+    detailsPanel.add(title);
 
     titleBox = new JTextField(post.title());
     titleBox.setBounds(110, 110, 350, 30);
-
-    detailsPanel.add(title);
     detailsPanel.add(titleBox);
   }
 
@@ -96,21 +94,10 @@ public class DetailsPopUp {
       if (post.password().equals(passwordBox.getText())) {
         postRepository.deletePost(post);
 
-        bulletinBoardPanel.removeAll();
-
-        bulletinBoardPanel.setLayout(new GridLayout(postRepository.postsSize() + 1, 1));
-
-        bulletinBoardPanel.initHeadline();
-        bulletinBoardPanel.initPostListsSection();
-
-        bulletinBoardPanel.setVisible(false);
-        bulletinBoardPanel.setVisible(true);
-
-        detailsFrame.setVisible(false);
+        refreshPanel();
       }
 
       passwordBox.setBackground(Color.PINK);
-
     });
 
     detailsPanel.add(delete);
@@ -122,26 +109,32 @@ public class DetailsPopUp {
     modify.addActionListener(event -> {
 
       if (post.password().equals(passwordBox.getText())) {
-        post = new Post(identifierBox.getText(), passwordBox.getText(), titleBox.getText(), contentBox.getText(), post.time());
+
+        post = new Post(identifierBox.getText(), passwordBox.getText(),
+            titleBox.getText(), contentBox.getText(), post.time());
+
         postRepository.modifyPost(post);
 
-        bulletinBoardPanel.removeAll();
-
-        bulletinBoardPanel.setLayout(new GridLayout(postRepository.postsSize() + 1, 1));
-
-        bulletinBoardPanel.initHeadline();
-        bulletinBoardPanel.initPostListsSection();
-
-        bulletinBoardPanel.setVisible(false);
-        bulletinBoardPanel.setVisible(true);
-
-        detailsFrame.setVisible(false);
+        refreshPanel();
       }
 
       passwordBox.setBackground(Color.PINK);
-
     });
 
     detailsPanel.add(modify);
+  }
+
+  public void refreshPanel() {
+    bulletinBoardPanel.removeAll();
+
+    bulletinBoardPanel.setLayout(new GridLayout(postRepository.postsSize() + 1, 1));
+
+    bulletinBoardPanel.initHeadline();
+    bulletinBoardPanel.initPostListsSection();
+
+    bulletinBoardPanel.setVisible(false);
+    bulletinBoardPanel.setVisible(true);
+
+    detailsFrame.setVisible(false);
   }
 }
